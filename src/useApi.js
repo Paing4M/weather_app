8b1d30b97d3ac8360964f88b7a8245e893872b20
Paing4M <paing.2000@yahoo.com>
2024-01-24 13:10:19 +0630

    some

--- src/useApi.js
+++ src/useApi.js
@@ -5,19 +5,19 @@ const useApi = (city) => {
 	const [{ weatherData }, dispatch] = useStateValue()
 
 	useEffect(() => {
-		// const weather = async () => {
-		// 	fetch(
-		// 		`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=907b3c7ab13841ab9192894cbb646424&units=metric`
-		// 	)
-		// 		.then((response) => response.json())
-		// 		.then((data) =>
-		// 			dispatch({
-		// 				type: 'ADD_DATA',
-		// 				weatherData: data,
-		// 			})
-		// 		)
-		// 		.catch((err) => console.log(err))
-		// }
+		const weather = async () => {
+			fetch(
+				`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=907b3c7ab13841ab9192894cbb646424&units=metric`
+			)
+				.then((response) => response.json())
+				.then((data) =>
+					dispatch({
+						type: 'ADD_DATA',
+						weatherData: data,
+					})
+				)
+				.catch((err) => console.log(err))
+		}
 
 		weather()
 	}, [city])
